ef1b9bddadabb8f864a8a92625548d0ade109abb
Michael Haller <mike.haller@gmail.com>
2023-07-09 20:08:30 -0400

    completed Eulers Sieve milestone

---
+++ Cryptography/EulersSieve.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func sieve_of_eratosthenes(max int) []bool {
+	is_prime := make([]bool, max+1)
+	is_prime[2] = true
+	for idx := 3; idx <= max; idx += 2 {
+		is_prime[idx] = true
+	}
+
+	for idx := 3; idx <= max; idx += 2 {
+		if is_prime[idx] {
+			for jdx := idx * 3; jdx <= max; jdx += idx {
+				is_prime[jdx] = false
+			}
+		}
+	}
+
+	return is_prime
+}
+
+func eulers_sieve(max int) []bool {
+	is_prime := make([]bool, max+1)
+	is_prime[2] = true
+	for idx := 3; idx <= max; idx += 2 {
+		is_prime[idx] = true
+	}
+
+	for idx := 3; idx <= max; idx += 2 {
+		if is_prime[idx] {
+			max_q := max / idx
+			if max_q%2 == 0 {
+				max_q--
+			}
+			for qdx := max_q; qdx >= idx; qdx -= 2 {
+				if is_prime[qdx] {
+					is_prime[idx*qdx] = false
+				}
+			}
+		}
+	}
+	return is_prime
+}
+
+func print_sieve(sieve []bool) {
+	fmt.Printf("2 ")
+
+	for idx := 3; idx < len(sieve); idx = idx + 2 {
+		if sieve[idx] {
+			fmt.Printf("%d ", idx)
+		}
+	}
+	fmt.Println()
+}
+
+func sieve_to_primes(slice []bool) []int {
+	primes := []int{2}
+
+	for idx := 3; idx < len(slice); idx = idx + 2 {
+		if slice[idx] {
+			primes = append(primes, idx)
+		}
+	}
+	return primes
+}
+
+func main() {
+	var max int
+	fmt.Printf("Max: ")
+	fmt.Scan(&max)
+
+	start := time.Now()
+	sieve := sieve_of_eratosthenes(max)
+	eulers := eulers_sieve(max)
+	elapsed := time.Since(start)
+	fmt.Printf("Elapsed: %f seconds\n", elapsed.Seconds())
+
+	if max <= 1000 {
+		print_sieve(sieve)
+		print_sieve(eulers)
+
+		primes := sieve_to_primes(sieve)
+		fmt.Println(primes)
+	}
+}
